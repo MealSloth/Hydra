851ce50cad0ee8868469c137629c59c5a00cfac4
Michael <michael@mealsloth.com>
2016-02-15 17:10:48 -0500

    Adding to urls

--- Hydra/urls.py
+++ Hydra/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
     url(r'^blob-image-upload/', views.blob_image_upload, name='blob-image-upload'),
+    url(r'^blog-image-upload/', views.blog_image_upload, name='blog-image-upload'),
 )
