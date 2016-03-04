a9c7ac08375fc9ef17012bb9aac44aeff996a21b
Michael <michael@mealsloth.com>
2016-03-04 02:57:59 -0500

    r inside quotes in regex.. just being silly

--- Hydra/urls.py
+++ Hydra/urls.py
@@ -5,7 +5,7 @@ import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='/'),
-    url('r^album/create/', views.album_create, name='album/create/'),
+    url(r'^album/create/', views.album_create, name='album/create/'),
     url(r'^album/delete/', views.album_delete, name='album/delete/'),
     url(r'^bucket/url/', views.bucket_url, name='bucket/url/'),
     url(r'^blob/upload/', views.blob_upload, name='blob/upload/'),
